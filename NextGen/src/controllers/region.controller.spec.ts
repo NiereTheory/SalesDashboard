6101dcc76538fd7c967acce43a62653f1b221493
Niere Theory <bniere@gmail.com>
2019-04-10 16:27:27 -0500

    prepping for unit testing

---
+++ NextGen/src/controllers/region.controller.spec.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+let baseUrl = 'http://localhost:3000/api/v1';
+
+describe('Test the root path', () => {
+
+    beforeAll(() => {
+    });
+
+    test('It should confirm a status of 200 for a GET response', async () => {
+        let res = await axios.get(`${baseUrl}/sales?startDate=2018-01-01&endDate=2018-01-31`);
+        expect(res.status).toEqual(200);
+    });
+
+    test('It should response the GET method', async () => {
+        let res = await axios.get(`${baseUrl}/sales?startDate=2018-01-01&endDate=2018-12-31`);
+        expect(res.data.data.length).toBeGreaterThan(100);
+    });
+
+    afterAll(() => {
+    });
+
+});
--- NextGen/src/server.ts
+++ NextGen/src/server.ts
@@ -1,28 +1,10 @@
 import 'reflect-metadata';
-import * as express from 'express';
-import * as morgan from 'morgan';
-import * as bodyParser from 'body-parser';
-import * as cors from 'cors';
 import { createConnection } from 'typeorm';
-
-import { SalesController } from './controllers/sales.controller';
-import { SellerController } from './controllers/seller.controller';
-import { RegionController } from './controllers/region.controller';
+import { app } from './app';
 
 const PORT = process.env.PORT || 3000;
 
 createConnection().then(async connection => {
-    const app = express();
-
-    app.use(bodyParser.json());
-    app.use(morgan('dev'));
-    app.use(cors());
-    app.disable('etag'); // remove general caching
-
-    app.use('/api/v1/sales', SalesController);
-    app.use('/api/v1/regions', RegionController);
-    app.use('/api/v1/sellers', SellerController);
-
     app.listen(PORT, () => console.log(`Running on ${PORT}`));
 }).catch(err => {
     console.log(`Connectivity issue: ${err}`);
